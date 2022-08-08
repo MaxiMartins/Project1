a96d4214fc0803dbcfae20ef8529f4096bf6e903
MaxiMartins <80002833+MaxiMartins@users.noreply.github.com>
2022-08-08 00:08:04 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        int resultado;
+        resultado = suma(10,20);
+        System.out.println(resultado);
+    }
+
+    public static int suma (int a,int b){
+        return (a + b);
+    }
+}
